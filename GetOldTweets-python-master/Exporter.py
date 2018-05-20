192bfd9644a7f179b4f1b69bf0090e0b16fe3a7b
megharangaswamy <meghagowda5193@gmail.com>
2018-05-20 07:44:02 +0200

    cleaning

---
+++ GetOldTweets-python-master/Exporter.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import sys,getopt,datetime,codecs
+if sys.version_info[0] < 3:
+    import got
+else:
+    import got3 as got
+
+def main(argv):
+
+	if len(argv) == 0:
+		print('You must pass some parameters. Use \"-h\" to help.')
+		return
+
+	if len(argv) == 1 and argv[0] == '-h':
+		f = open('exporter_help_text.txt', 'r')
+		print(f.read()) 
+		f.close()
+
+		return
+
+	try:
+		opts, args = getopt.getopt(argv, "", ("username=", "near=", "within=", "since=", "until=", "querysearch=", "toptweets", "maxtweets=", "output=","lang="))
+
+		tweetCriteria = got.manager.TweetCriteria()
+		outputFileName = "output_got.csv"
+
+		for opt,arg in opts:
+			if opt == '--username':
+				tweetCriteria.username = arg
+
+			elif opt == '--since':
+				tweetCriteria.since = arg
+
+			elif opt == '--until':
+				tweetCriteria.until = arg
+
+			elif opt == '--querysearch':
+				tweetCriteria.querySearch = arg
+
+			elif opt == '--toptweets':
+				tweetCriteria.topTweets = True
+
+			elif opt == '--maxtweets':
+				tweetCriteria.maxTweets = int(arg)
+			
+			elif opt == '--near':
+				tweetCriteria.near = '"' + arg + '"'
+			
+			elif opt == '--within':
+				tweetCriteria.within = '"' + arg + '"'
+
+			elif opt == '--within':
+				tweetCriteria.within = '"' + arg + '"'
+
+			elif opt == '--output':
+				outputFileName = arg
+			elif opt == '--lang':
+				outputFileName = arg
+				
+		outputFile = codecs.open(outputFileName, "w+", "utf-8")
+
+		#outputFile.write('username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink')
+		#MR
+		outputFile.write('idµdateµtextµpermalink')
+
+		print('Searching...\n')
+
+		def receiveBuffer(tweets):
+			for t in tweets:
+				#MR
+				#outputFile.write(('\n"%s";%d;%d;"%s";%s;%s;%s;"%s";%s' % ( t.id,t.date.strftime("%Y-%m-%d %H:%M"), t.text, t.hashtags, t.permalink)))
+				outputFile.write(('\n%sµ%sµ%sµ%s' % (t.id, t.date.strftime("%Y-%m-%d %H:%M"), t.text, t.permalink)))
+			outputFile.flush()
+			print('More %d saved on file...\n' % len(tweets))
+
+		got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
+
+	except arg:
+		print('Arguments parser error, try -h' + arg)
+	finally:
+		outputFile.close()
+		print('Done. Output file generated "%s".' % outputFileName)
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
---
+++ README.md
@@ -0,0 +1,8 @@
+# twitter_Web_Mining
+
+1) command when crawling data for iphone : python Exporter.py --since 2017-12-01 --until 2018-01-01 --querysearch "iphone and phone" --toptweets (change the dates accordingly)
+2) command when crawling data for lenovo : python Exporter.py --since 2017-11-01 --until 2017-12-01 --querysearch "lenovo and phone" --toptweets (change the dates accordingly)
+
+# WM2.1 Sentiment Annotation.py
+
+1) client = textapi.Client("App ID", "Key") (enter your own APP ID and Key got on https://developer.aylien.com
